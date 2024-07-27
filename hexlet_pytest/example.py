812dd864c00fc71e4f39be7a70f3693a4022fcd4
AntiViruS90 <slava90nikitin90@gmail.com>
2024-07-27 15:09:25 +0300

    Add new files in hexlet_pytest

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,18 @@
 def reverse(string):
+    """
+    :param string:
+    Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+
+    """
     return string[::-1]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
